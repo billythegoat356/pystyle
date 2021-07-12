91404b9ab328c7a5505d46fb59270a83c8418cc3
Billy <77754159+billythegoat356@users.noreply.github.com>
2021-07-13 00:28:20 +0200

    Update __init__.py

--- pycenter/__init__.py
+++ pycenter/__init__.py
@@ -14,9 +14,7 @@ from os import get_terminal_size
 
 def center(var, space=None):
 
-    size = get_terminal_size().columns
-
     if not space:
-        space = (size - len(var.splitlines()[int(len(var.splitlines())/2)])) / 2
+        space = (get_terminal_size().columns - len(var.splitlines()[int(len(var.splitlines())/2)])) / 2
     
     return "".join(f"{(' ' * int(space)) + var}\n" for var in var.splitlines())
