7e44ee0654ca0b3418b4ae95571257d2d47f5c7f
Billy <77754159+billythegoat356@users.noreply.github.com>
2021-08-28 21:22:09 +0200

    Update __init__.py

--- pycenter/__init__.py
+++ pycenter/__init__.py
@@ -16,7 +16,7 @@ def center(var: str, space: int = None, icon: str = " ", sep: bool = False):
     if space is None:
         space = (get_terminal_size().columns - len(var.splitlines()[int(len(var.splitlines())/2)])) / 2
     
-    if sep:
+    if not sep:
         return "\n".join((icon * int(space)) + var for var in var.splitlines())
     else:
-        return "\n".join((icon * int(space)) + var + icon * int(space) for var in var.splitlines())
+        return "\n".join((icon * int(space)) + var + (icon * int(space)) for var in var.splitlines())
