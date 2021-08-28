93212691384d32f7a022d88ec551c7ac530170af
Billy <77754159+billythegoat356@users.noreply.github.com>
2021-08-28 21:18:31 +0200

    Update __init__.py

--- pycenter/__init__.py
+++ pycenter/__init__.py
@@ -2,7 +2,7 @@
 
 # https://github.com/billythegoat356
 
-# Version : 1.6
+# Version : 1.7
 
 # <3
 
@@ -11,9 +11,12 @@ from os import get_terminal_size
 
 
 
-def center(var: str, space: int=None, icon: str = " "):
+def center(var: str, space: int = None, icon: str = " ", sep: bool = False):
 
     if space is None:
         space = (get_terminal_size().columns - len(var.splitlines()[int(len(var.splitlines())/2)])) / 2
     
-    return "\n".join((icon * int(space)) + var for var in var.splitlines())
+    if sep:
+        return "\n".join((icon * int(space)) + var for var in var.splitlines())
+    else:
+        return "\n".join((icon * int(space)) + var + icon * int(space) for var in var.splitlines())
