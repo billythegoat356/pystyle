4852e09cf9d82fcd714740e7b48774b899da1da1
Billy <77754159+billythegoat356@users.noreply.github.com>
2021-09-03 21:06:34 +0200

    Update __init__.py

--- pycenter/__init__.py
+++ pycenter/__init__.py
@@ -2,7 +2,7 @@
 
 # https://github.com/billythegoat356
 
-# Version : 1.7
+# Version : 2.0
 
 # <3
 
@@ -19,4 +19,21 @@ def center(var: str, space: int = None, icon: str = " ", sep: bool = False):
     if not sep:
         return "\n".join((icon * int(space)) + var for var in var.splitlines())
     else:
-        return "\n".join((icon * int(space)) + var + (icon * int(space)) for var in var.splitlines())
+        return "\n".join((icon * int(space)) + var + icon * int(space) for var in var.splitlines())
+
+
+def makebox(content: str):
+    l = 0
+    lines = content.splitlines()
+    for a in lines:
+        if len(a) > l:
+            l = len(a)
+    if l % 2 == 1:
+        l += 1
+    box = "__" + ("_" * l) + "__\n"
+    box += "| " + (" " * int(l / 2)) + (" " * int(l / 2)) + " |\n"
+    for line in lines:
+        box += "| " + line + (" " * int((l - len(line)))) + " |\n"
+    box += "|_" + ("_" * l) + "_|\n"
+
+    return box
