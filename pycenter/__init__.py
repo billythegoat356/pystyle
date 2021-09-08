7e3969652f839181d576b4d6303c1251b7923922
Billy <77754159+billythegoat356@users.noreply.github.com>
2021-09-08 16:32:05 +0200

    Update __init__.py

--- pycenter/__init__.py
+++ pycenter/__init__.py
@@ -4,12 +4,18 @@
 
 # <3
 
+
+from os import get_terminal_size
+
+
 def center(var: str, space: int = None, icon: str = " ", sep: bool = False):
+    if not space:
+        space = (get_terminal_size().columns - len(var.splitlines()[int(len(var.splitlines())/2)])) / 2
+
     if not sep:
         return "\n".join((icon * int(space)) + var for var in var.splitlines())
     else:
         return "\n".join((icon * int(space)) + var + (icon * int(space)) for var in var.splitlines())
-        return "\n".join((icon * int(space)) + var + icon * int(space) for var in var.splitlines())
 
 
 def makebox(content: str):
